8c8b2db99358b0bcbf313dbb140d9979e0e39341
10230939 <10230939@D205PC30>
2026-01-15 18:09:20 +0300

    Log&Reg

---
+++ AppCulinaryBook/AppData/AppConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCulinaryBook.AppData
+{
+    internal class AppConnect
+    {
+        public static Culinary_BookEntities1 culinary_book_entities1;
+    }
+}
---
+++ AppCulinaryBook/Pages/PageAuthorization.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppCulinaryBook.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAuthorization.xaml
+    /// </summary>
+    public partial class PageAuthorization : Page
+    {
+        public PageAuthorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = AppData.AppConnect.culinary_book_entities1.Authors.FirstOrDefault(x =>
+                x.Login == LoginTxT.Text && x.Password == PasswordTxT.Password);
+                if (userObj == null)
+                {
+                    MessageBox.Show("Такого пользователя нет", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Здравствуйте,Автор " + userObj.AuthorName + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка" + ex.Message.ToString(),"Критическая ошибка приложения", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
